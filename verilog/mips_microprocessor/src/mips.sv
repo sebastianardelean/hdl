2622fdef31ec17c90a44a298e33a833ff2a22b2b
Sebastian Ardelean <37292233+sebastianardelean@users.noreply.github.com>
2022-04-29 15:54:05 +0300

    Added all instructions and basic simulation is working

--- verilog/mips_microprocessor/src/mips.sv
+++ verilog/mips_microprocessor/src/mips.sv
@@ -72,32 +72,32 @@ module maindec(input  logic [5:0] op,
 
   always_comb
     case(op)
-      6'b000000: controls = 16'b1100000000000010; //Rtype
-      6'b000001: controls = 16'b0000100000000110; //bltz
-      6'b000010: controls = 16'b0000000100000000; //j
-      6'b000011: controls = 16'b0000000100000000; //jal
-      6'b000100: controls = 16'b0000100000000101; //beq
-      6'b000101: controls = 16'b0000100000001001; //bne
-      6'b000110: controls = 16'b0000100000001010; //blez
-      6'b000111: controls = 16'b0000100000001011; //bgtz
-      6'b001000: controls = 16'b1001000000001100; //addi
-      6'b001001: controls = 16'b1001000000001101; //addiu
-      6'b001010: controls = 16'b1001000000001110; //slti
-      6'b001011: controls = 16'b1001000000001111; //sltiu
-      6'b001100: controls = 16'b1001000000010000; //andi
-      6'b001101: controls = 16'b1001000000010001; //ori
-      6'b001110: controls = 16'b1001000000010010; //xori
-      6'b001111: controls = 16'b1010000000010011; //lui
-      6'b011100: controls = 16'b1000000000010100; //mul
-      6'b100000: controls = 16'b1001001000010101; //lb
-      6'b100001: controls = 16'b1001001000010110; //lh
-      6'b100011: controls = 16'b1001001000000011; //lw
-      6'b100100: controls = 16'b1001001000010111; //lbu
-      6'b100101: controls = 16'b1001001000011000; //lhu
-      6'b101000: controls = 16'b0001010000011001; //sb
-      6'b101001: controls = 16'b0001010000011010; //sh
-      6'b101011: controls = 16'b0001010000000100; //sw
-      default:   controls = 16'bxx0xxxxxxxxxxxxx;
+        6'b000000: controls <= 16'b1100000000000010; //r-type
+        6'b100011: controls <= 16'b1001001000000011; //lw      ADD  
+        6'b101011: controls <= 16'b0001010000000100; //sw      ADD  
+        6'b000100: controls <= 16'b0000100000000101; //beq     SUB  
+        6'b000001: controls <= 16'b0000100000000110; //bltz    SUB  
+        6'b000010: controls <= 16'b0000000100000111; //j       NOP  
+        6'b000011: controls <= 16'b0000000100001000; //jal     NOP  
+        6'b000101: controls <= 16'b0000100000001001; //bne     SUB  
+        6'b000110: controls <= 16'b0000100000001010; //blez    SUB  
+        6'b000111: controls <= 16'b0000100000001011; //bgtz    SUB  
+        6'b001000: controls <= 16'b1001000000001100; //addi    ADD  
+        6'b001001: controls <= 16'b1001000000001101; //addiu   ADD  
+        6'b001010: controls <= 16'b1001000000001110; //slti    SET  
+        6'b001011: controls <= 16'b1001000000001111; //sltiu   SET  
+        6'b001100: controls <= 16'b1001000000010000; //andi    AND  
+        6'b001101: controls <= 16'b1001000000010001; //ori     OR   
+        6'b001110: controls <= 16'b1101000000010010; //xori    XOR  
+        6'b001111: controls <= 16'b1010000000010011; //lui     NOP  
+        6'b011100: controls <= 16'b1100000000010100; //mul     MUL32
+        6'b100000: controls <= 16'b1001001000010101; //lb      ADD  
+        6'b100001: controls <= 16'b1001001000010110; //lh      ADD  
+        6'b100100: controls <= 16'b1001001000010111; //lbu     ADD  
+        6'b100101: controls <= 16'b1001000000011000; //lhu     ADD  
+        6'b101000: controls <= 16'b0001010000011001; //sb      ADD  
+        6'b101001: controls <= 16'b0001010000011010; //sh      ADD  
+        default:   controls <= 16'bxx0xxxxxxxxxxxxx;
     endcase
 endmodule
 
@@ -108,58 +108,58 @@ module aludec(input  logic [5:0] funct,
 
   always_comb
     case(aluop)
-	8'b00000011: alucontrol = 4'b0000;//LW
-	8'b00000100: alucontrol = 4'b0001;//SW
-	8'b00000101: alucontrol = 4'b0001;//BEQ
-	8'b00000110: alucontrol = 4'b0001;//BLTZ
-	8'b00001001: alucontrol = 4'b0001;//bne
-	8'b00001010: alucontrol = 4'b0001;//blez
-	8'b00001011: alucontrol = 4'b0001;//bgtz
-	8'b00001100: alucontrol = 4'b0000;//addi
-	8'b00001101: alucontrol = 4'b0000;//addiu
-	8'b00001110: alucontrol = 4'b0100;//slti
-	8'b00001111: alucontrol = 4'b0100;//sltiu
-	8'b00010000: alucontrol = 4'b0101;//andi
-	8'b00010001: alucontrol = 4'b0110;//ori
-	8'b00010010: alucontrol = 4'b0111;//xori
-	8'b00010011: alucontrol = 4'b1111;//lui 
-	8'b00010100: alucontrol = 4'b0010;//mul64
-	8'b00010101: alucontrol = 4'b0000;//lb
-	8'b00010110: alucontrol = 4'b0000;//lh
-	8'b00010111: alucontrol = 4'b0000;//lbu
-	8'b00011000: alucontrol = 4'b0000;//lhu
-	8'b00011001: alucontrol = 4'b0000;//sb
-	8'b00011010: alucontrol = 4'b0000;//sh
+	8'b00000011: alucontrol <= 5'b00000;//LW
+	8'b00000100: alucontrol <= 5'b00000;//SW
+	8'b00000101: alucontrol <= 5'b00001;//BEQ
+	8'b00000110: alucontrol <= 5'b00001;//BLTZ
+	8'b00001001: alucontrol <= 5'b00001;//bne
+	8'b00001010: alucontrol <= 5'b00001;//blez
+	8'b00001011: alucontrol <= 5'b00001;//bgtz
+	8'b00001100: alucontrol <= 5'b00000;//addi
+	8'b00001101: alucontrol <= 5'b00000;//addiu
+	8'b00001110: alucontrol <= 5'b01001;//slti
+	8'b00001111: alucontrol <= 5'b01001;//sltiu
+	8'b00010000: alucontrol <= 5'b00101;//andi
+	8'b00010001: alucontrol <= 5'b00110;//ori
+	8'b00010010: alucontrol <= 5'b00111;//xori
+	8'b00010011: alucontrol <= 5'b10000;//lui 
+	8'b00010100: alucontrol <= 5'b00010;//mul32
+	8'b00010101: alucontrol <= 5'b00000;//lb
+	8'b00010110: alucontrol <= 5'b00000;//lh
+	8'b00010111: alucontrol <= 5'b00000;//lbu
+	8'b00011000: alucontrol <= 5'b00000;//lhu
+	8'b00011001: alucontrol <= 5'b00000;//sb
+	8'b00011010: alucontrol <= 5'b00000;//sh
 	8'b00000010: case(funct)          // RTYPE
-			6'b000000: alucontrol = 4'b1001;  // SLL
-		        6'b000010: alucontrol = 4'b1010;  // SRL
-		        6'b000011: alucontrol = 4'b1100;  // SRA
-		        6'b000100: alucontrol = 4'b1001;  // SLLV
-		        6'b000110: alucontrol = 4'b1010;  // SRLV
-		        6'b000111: alucontrol = 4'b1100;  // SRAV
-			6'b001000: alucontrol = 4'b1111;  // JR
-			6'b001001: alucontrol = 4'b1111;  // JALR
-			6'b001100: alucontrol = 4'b1111;  // SYSCALL
-			6'b001101: alucontrol = 4'b1111;  // BREAK
-			6'b010000: alucontrol = 4'b1111;  // MFHI
-		        6'b010001: alucontrol = 4'b1111;  // MTHI
-			6'b010010: alucontrol = 4'b1111;  // MFLO
-		        6'b010011: alucontrol = 4'b1111;  // MTLO
-			6'b011000: alucontrol = 4'b0011;  // MUL
-			6'b011001: alucontrol = 4'b0011;  // MULU
-			6'b011010: alucontrol = 4'b1011;  // DIV
-			6'b011011: alucontrol = 4'b1011;  // DIVU
-		        6'b100000: alucontrol = 4'b0000; // ADD
-			6'b100001: alucontrol = 4'b0000;  // ADDU
-			6'b100010: alucontrol = 4'b0001; // SUB
-			6'b100011: alucontrol = 4'b0001;  // SUBU
-			6'b100100: alucontrol = 4'b0101;  // AND
-			6'b100101: alucontrol = 4'b0110;  // OR
-			6'b100110: alucontrol = 4'b0111;  // XOR
-			6'b100111: alucontrol = 4'b1000;  // NOR
-			6'b101010: alucontrol = 4'b0100;  // SLT
-			6'b101011: alucontrol = 4'b0100;  // SLTU
-			default:   alucontrol = 4'bxxxx; // ???
+			6'b000000: alucontrol <= 5'b01010;  // SLL
+		        6'b000010: alucontrol <= 5'b01011;  // SRL
+		        6'b000011: alucontrol <= 5'b01100;  // SRA
+		        6'b000100: alucontrol <= 5'b01101;  // SLLV
+		        6'b000110: alucontrol <= 5'b01110;  // SRLV
+		        6'b000111: alucontrol <= 5'b01111;  // SRAV
+			6'b001000: alucontrol <= 5'b11111;  // JR
+			6'b001001: alucontrol <= 5'b11111;  // JALR
+			6'b001100: alucontrol <= 5'b11111;  // SYSCALL
+			6'b001101: alucontrol <= 5'b11111;  // BREAK
+			6'b010000: alucontrol <= 5'b11111;  // MFHI
+		        6'b010001: alucontrol <= 5'b11111;  // MTHI
+			6'b010010: alucontrol <= 5'b11111;  // MFLO
+		        6'b010011: alucontrol <= 5'b11111;  // MTLO
+			6'b011000: alucontrol <= 5'b00011;  // MUL64
+			6'b011001: alucontrol <= 5'b00011;   // MUL64
+			6'b011010: alucontrol <= 5'b00100;  // DIV
+			6'b011011: alucontrol <= 5'b00100;  // DIVU
+		        6'b100000: alucontrol <= 5'b00000; // ADD
+			6'b100001: alucontrol <= 5'b00000;  // ADDU
+			6'b100010: alucontrol <= 5'b00001; // SUB
+			6'b100011: alucontrol <= 5'b00001;  // SUBU
+			6'b100100: alucontrol <= 5'b00101;  // AND
+			6'b100101: alucontrol <= 5'b00110;  // OR
+			6'b100110: alucontrol <= 5'b00111;  // XOR
+			6'b100111: alucontrol <= 5'b01000;  // NOR
+			6'b101010: alucontrol <= 5'b01001;  // SLT
+			6'b101011: alucontrol <= 5'b01001;  // SLTU
+			default:   alucontrol <= 5'bxxxxx; // ???
 		      endcase
     endcase
 endmodule
@@ -208,7 +208,7 @@ module datapath(input  logic        clk, reset,
   mux3 #(32)  srcbmux(writedata, signimm, signimmsh16,
 		      alusrc,
                       srcb);
-  alu         alu(.a(srca), .b(srcb), .f(alucontrol),
+  alu         alu(.a(srca), .b(srcb), .shamt(instr[10:6]),.f(alucontrol),
                   .result(aluout), .zero(zero));
 endmodule
 
@@ -219,27 +219,28 @@ module alu(input  logic [31:0] a, b,
 	   output logic        zero);
     always_comb
 	case (f)
-          4'b0000: result = a + b;
-	  4'b0001: result = a - b;
-	  4'b0010: result = a * b;
-	  4'b0011: result = a * b;
-	  4'b0100: if (a<b) 
+	    5'b00000: result <= a + b;
+	    5'b00001: result <= a - b;
+	    5'b00010: result <= a * b;
+	    5'b00011: result <= a * b;
+	    5'b00100: result <= a / b;
+	    5'b00101: result <= a & b;
+	    5'b00110: result <= a | b;
+	    5'b00111: result <= a ^ b;
+	    5'b01000: result <= ~(a | b);
+	    5'b01001: if (a < b) 
 			result = 1'b1;
-		   else
-			result = 1'b0;
-	  4'b0101: result = a & b;
-	  4'b0110: result = a | b; 
-	  4'b0111: result = a ^ b;
-	  4'b1000: result = ~ (a | b);
-	  4'b1001: result = a << b;
-	  4'b1010: result = a >> b;
-	  4'b1011: result = a/b; 
-	  4'b1100: result = a >>> b;
-	  4'b1111: result = b;
-	  default: result = 31'bx;
-//	  4'b1101:
-//	  4'b1110:
-//	  4'b1111: 
+		      else
+		        result = 1'b0;
+	    5'b01010: result <= b << shamt;
+	    5'b01011: result <= b >> shamt;
+	    5'b01100: result <= b >>> shamt;
+	    5'b01101: result <= a << b;
+	    5'b01110: result <= a >> b;
+	    5'b01111: result <= a >>> b;
+	    5'b10000: result <= b;
+	    default: result = 31'bx;
 	endcase
+    assign zero = (result == 32'b0);
 endmodule
 
